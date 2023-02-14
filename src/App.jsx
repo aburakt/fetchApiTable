e8db3593dc3908c9d41976909b97a4d3f5bd43bc
aBurakt <info@aburakt.com>
2023-02-14 17:20:25 +0300

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ export default function App() {
 	const [data, setData] = useState([]);
 
 	const fetchData = () => {
-		// fetch(`https://dummyjson.com/products`)
 		fetch(`https://api.orhanaydogdu.com.tr/deprem/kandilli/live`)
 			.then((response) => response.json())
 			.then((actualData) => {
@@ -29,31 +28,16 @@ export default function App() {
 					<th>Enlem Boylam</th>
 					<th>Date</th>
 				</tr>
-
-
 				{Object.entries(data).map((subject, i) => {
-					console.log(subject);
 					return (
 						<tr key={i}>
-							<td >{subject[1].title}</td>
-							<td style={{textAlign:"center"}}>{subject[1].mag}</td>
+							<td>{subject[1].title}</td>
+							<td style={{ textAlign: "center" }}>{subject[1].mag}</td>
 							<td>{subject[1].lat} {subject[1].lng}</td>
 							<td>{subject[1].date}</td>
 						</tr>
 					);
 				})}
-
-				{/* {data.map((item, index) => (
-					<tr key={index}>
-						<td>{item.title[0]}</td>
-						<td>{item.brand}</td>
-						<td>
-							<img src={item.thumbnail} alt="" height={100} />
-						</td>
-						<td>{item.price}</td>
-						<td>{item.rating}</td>
-					</tr>
-				))} */}
 			</tbody>
 		</div>
 	);
